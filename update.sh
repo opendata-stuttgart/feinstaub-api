69476e7d2c9af21ae583d15b66622641c28bf4e2
Andreas Madsack <andreas@madflex.de>
2015-03-31 20:46:08 +0200

    restart always

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 git pull
 docker build --tag=feinstaub-prod .
 docker rm -f feinstaub feinstaub-nginx
-docker run -d --volumes-from feinstaub-data --link feinstaub-db:db --name feinstaub feinstaub-prod
-docker run --name feinstaub-nginx --net="host" --volumes-from feinstaub-data -v `pwd`/nginx.conf:/etc/nginx/nginx.conf -d nginx
+docker run -d --volumes-from feinstaub-data --link feinstaub-db:db --restart=always --name feinstaub feinstaub-prod
+docker run --name feinstaub-nginx --net="host" --volumes-from feinstaub-data -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --restart=always -d nginx
