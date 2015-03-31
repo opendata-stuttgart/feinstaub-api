cc373d334d198893c873dfdc1c75e94a2bede82d
Andreas Madsack <andreas@madflex.de>
2015-03-31 19:10:37 +0200

    update for uwsgi and sockets

--- feinstaub/start.sh
+++ feinstaub/start.sh
@@ -3,6 +3,7 @@ export DJANGO_SETTINGS_MODULE=feinstaub.settings.production
 export PYTHONUNBUFFERED=0
 mkdir -p /home/uid1000/feinstaub
 mkdir -p /home/uid1000/feinstaub/logs
+mkdir -p /home/uid1000/feinstaub/run
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-exec uwsgi --http 0.0.0.0:8000 --wsgi-file /opt/code/feinstaub/feinstaub/wsgi.py --master --processes 4 --threads 2
+uwsgi --socket /home/uid1000/feinstaub/run/server.sock --wsgi-file /opt/code/feinstaub/feinstaub/wsgi.py --master --processes 4 --threads 2
