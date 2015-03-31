dd4f2f3a5966f2dc19782d0c88d5b5aca53c3ea8
Andreas Madsack <andreas@madflex.de>
2015-03-31 19:19:54 +0200

    not needed in start.sh

--- feinstaub/start.sh
+++ feinstaub/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=feinstaub.settings.production
 export PYTHONUNBUFFERED=0
-mkdir -p /home/uid1000/feinstaub
-mkdir -p /home/uid1000/feinstaub/logs
-mkdir -p /home/uid1000/feinstaub/run
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 uwsgi --socket /home/uid1000/feinstaub/run/server.sock --wsgi-file /opt/code/feinstaub/feinstaub/wsgi.py --master --processes 4 --threads 2
