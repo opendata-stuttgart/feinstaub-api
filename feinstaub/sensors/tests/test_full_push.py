65d7c38a702cafff0cbab5572c9ac552964411e9
Andreas Madsack <andreas@madflex.de>
2015-04-06 17:44:21 +0200

    tests for nested_write and a full_push test

---
+++ feinstaub/sensors/tests/test_full_push.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+from rest_framework.test import APIRequestFactory
+import pytest
+import pytz
+
+from sensors.views import SensorDataView
+from sensors.models import SensorData
+
+
+@pytest.mark.django_db
+class TestSensorDataPushFull:
+
+    @pytest.fixture
+    def data_fixture(self):
+        return {
+            "sampling_rate": "15000",
+            "timestamp": "2015-04-05 22:10:10+02:00",
+            "sensordatavalues": [{"value": 10, "value_type": "P1"},
+                                 {"value": 99, "value_type": "P2"}]
+        }
+
+    def test_full_data_push(self, sensor, data_fixture):
+        factory = APIRequestFactory()
+        view = SensorDataView
+        url = '/v1/push-sensor-data/'
+        request = factory.post(url, data_fixture,
+                               format='json')
+
+        # authenticate sensor
+        request.META['HTTP_SENSOR'] = sensor.uid
+
+        view_function = view.as_view({'post': 'create'})
+        response = view_function(request)
+
+        assert response.status_code == 201
+
+        sd = SensorData.objects.get(sensor=response.data['sensor'])
+
+        assert sd.sensordatavalues.count() == 2
+        assert sd.sensordatavalues.get(value_type="P1").value ==\
+            str(data_fixture['sensordatavalues'][0]['value'])
+        assert sd.sensordatavalues.get(value_type="P2").value ==\
+            str(data_fixture['sensordatavalues'][1]['value'])
+
+        assert sd.location == sensor.location
+
+        cest = pytz.timezone('Europe/Berlin')
+        assert str(cest.normalize(sd.timestamp)) == data_fixture['timestamp']
---
+++ feinstaub/sensors/tests/test_nested_write_sensordata_push.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+from rest_framework.test import APIRequestFactory
+import pytest
+
+from sensors.views import SensorDataView
+
+
+@pytest.mark.django_db
+class TestSensorDataPush:
+
+    @pytest.fixture(params=[
+        # value, status_code, count
+        [[], 201, 0],
+        [[{"value": 10, "value_type": "P1"}], 201, 1],
+        [[{"value": 10, "value_type": "P1"}, {"value": 99, "value_type": "P2"}], 201, 2],
+        ## failes:
+        ['INVALID', 400, 1],
+        [['INVALID'], 400, 1],
+        [[{'INVALID_KEY': 1}], 400, 1],
+        ])
+    def sensordatavalue_fixture(self, request):
+        keys = ["value", "status_code", "count"]
+        return dict(zip(keys, request.param))
+
+    def test_sensordata_push(self, sensor, sensordatavalue_fixture):
+        factory = APIRequestFactory()
+        view = SensorDataView
+        url = '/v1/push-sensor-data/'
+        request = factory.post(url, {'sensordatavalues':
+                                     sensordatavalue_fixture.get('value'), },
+                               format='json')
+
+        request.META['HTTP_SENSOR'] = sensor.uid
+
+        view_function = view.as_view({'post': 'create'})
+        response = view_function(request)
+
+        assert len(response.data.get('sensordatavalues')) ==\
+            sensordatavalue_fixture.get('count')
+
+        assert response.status_code ==\
+            sensordatavalue_fixture.get('status_code')
