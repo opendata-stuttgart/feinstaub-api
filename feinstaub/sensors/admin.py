92d3c586f49fbdd96b290a00f40771d119f4c364
Andreas Madsack <andreas@madflex.de>
2015-04-06 18:39:25 +0200

    fields, filters and search_fields for sensor models in admin
    fixes #19

--- feinstaub/sensors/admin.py
+++ feinstaub/sensors/admin.py
@@ -11,24 +11,38 @@ from .models import (
 
 @admin.register(Sensor)
 class SensorAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['uid', 'description']
+    list_display = ['uid', 'owner', 'sensor_type', 'location',
+                    'description', 'created', 'modified']
+    list_filter = ['owner', 'sensor_type', 'location']
 
 
 @admin.register(SensorData)
 class SensorDataAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['sensor__uid', ]
+    list_display = ['sensor', 'sampling_rate', 'timestamp',
+                    'location', 'created', 'modified']
+    list_filter = ['sensor', 'location', 'sensor__sensor_type']
 
 
 @admin.register(SensorDataValue)
 class SensorDataValueAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['sensordata', 'value_type', 'value',
+                    'created', 'modified']
+    list_filter = ['value_type', 'sensordata__sensor',
+                   'sensordata__sensor__sensor_type']
 
 
 @admin.register(SensorLocation)
 class SensorLocationAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['location', ]
+    list_display = ['location', 'indoor', 'owner', 'description',
+                    'timestamp', 'created', 'modified']
+    list_filter = ['indoor', 'owner']
 
 
 @admin.register(SensorType)
 class SensorTypeAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['uid', 'name', 'manufacturer', 'description']
+    list_display = ['uid', 'name', 'manufacturer',
+                    'description', 'created', 'modified']
