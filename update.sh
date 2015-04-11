1f520cd4dab938b6c62010886630caea7d905140
Andreas Madsack <andreas@madflex.de>
2015-04-11 17:04:43 +0200

    fix path

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -e
 git pull
 docker build --tag=feinstaub-prod .
 docker rm -f feinstaub feinstaub-nginx
-docker run -d --volumes-from feinstaub-data --link feinstaub-db:db --link feinstaub-redis:redis -v feinstaub/settings/production.py /opt/code/feinstaub/feinstaub/settings/production.py --restart=always --name feinstaub feinstaub-prod
+docker run -d --volumes-from feinstaub-data --link feinstaub-db:db --link feinstaub-redis:redis -v `pwd`/feinstaub/settings/production.py /opt/code/feinstaub/feinstaub/settings/production.py --restart=always --name feinstaub feinstaub-prod
 docker run --name feinstaub-nginx --net="host" --volumes-from feinstaub-data -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --restart=always -d nginx
 
 echo "Cleaning up old docker images..."
