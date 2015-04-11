1fc721a330338135f51b673e9cc3a12e871a05da
Andreas Madsack <andreas@madflex.de>
2015-04-11 12:13:11 +0200

    add settings for tests on wercker

--- feinstaub/feinstaub/settings/test.py
+++ feinstaub/feinstaub/settings/test.py
@@ -7,3 +7,17 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
+
+
+if os.environ.get('TEST_ON_PLATFORM') == 'wercker':
+    DATABASES = {
+        'default': {
+            'HOST': os.environ.get('WERCKER_POSTGRESQL_HOST', '127.0.01'),
+            'NAME': os.environ.get('WERCKER_POSTGRESQL_DATABASE', 'test'),
+            'TEST_NAME': os.environ.get('WERCKER_POSTGRESQL_DATABASE', 'test'),
+            'USER': os.environ.get('WERCKER_POSTGRESQL_USERNAME', 'postgres'),
+            'PASSWORD': os.environ.get('WERCKER_POSTGRESQL_PASSWORD', ''),
+            'HOST': os.environ.get('WERCKER_POSTGRESQL_HOST', '127.0.01'),
+            'PORT': os.environ.get('WERCKER_POSTGRESQL_PORT', '5432'),
+        }
+    }
