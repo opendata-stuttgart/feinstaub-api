93ac96235cb96c9210f3dad80735ab0213d30b9f
Andreas Madsack <andreas@madflex.de>
2015-06-29 00:18:40 +0200

    add view to generate image for kindle. code is from another project of me. needs a bit more work here.

--- feinstaub/sensors/urls.py
+++ feinstaub/sensors/urls.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 from rest_framework import routers
 from django.conf.urls import patterns, include, url
-from .views import SensorDataView, StatisticsView, CacheView
+from .views import SensorDataView, StatisticsView, CacheView, KindleView
 
 
 router = routers.DefaultRouter()
 router.register(r'push-sensor-data', SensorDataView)
 router.register(r'statistics', StatisticsView, base_name='statistics')
 router.register(r'cache', CacheView, base_name='cache')
+router.register(r'kindle', KindleView, base_name='kindle')
 
 
 urlpatterns = patterns(
