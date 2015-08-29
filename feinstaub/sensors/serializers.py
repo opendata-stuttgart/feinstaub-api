c26bc90d5a6b814a55ce94a001ac971473af1dc9
Andreas Madsack <andreas@madflex.de>
2015-08-29 20:43:59 +0200

    optimize serializers to show lots of nested stuff

--- feinstaub/sensors/serializers.py
+++ feinstaub/sensors/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import exceptions, serializers
 
-from .models import SensorData, SensorDataValue, Node
+from .models import (
+    Node,
+    Sensor,
+    SensorData,
+    SensorDataValue,
+    SensorLocation,
+    SensorType,
+)
 
 
 class SensorDataValueSerializer(serializers.ModelSerializer):
@@ -51,6 +58,50 @@ class SensorDataSerializer(serializers.ModelSerializer):
         return sd
 
 
+class NestedSensorDataSerializer(serializers.ModelSerializer):
+    sensordatavalues = SensorDataValueSerializer(many=True)
+
+    class Meta:
+        model = SensorData
+        fields = ('sensor', 'sampling_rate', 'timestamp', 'sensordatavalues')
+        read_only = ('location')
+
+
+class NestedSensorLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SensorLocation
+        fields = ('id', "location", "indoor", "description")
+
+
+class NestedSensorTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SensorType
+        fields = ('id', "name", "manufacturer")
+
+
+class NestedSensorSerializer(serializers.ModelSerializer):
+    sensor_type = NestedSensorTypeSerializer()
+    sensordatas = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Sensor
+        fields = ('id', 'description', 'pin', 'sensor_type', 'sensordatas')
+
+    def get_sensordatas(self, obj):
+        sensordatas = SensorData.objects.filter(sensor=obj).order_by('-timestamp')[:2]
+        serializer = NestedSensorDataSerializer(instance=sensordatas, many=True)
+        return serializer.data
+
+
 class NodeSerializer(serializers.ModelSerializer):
+    sensors = NestedSensorSerializer(many=True)
+    location = NestedSensorLocationSerializer()
+    last_data_push = serializers.SerializerMethodField()
+
     class Meta:
         model = Node
+        fields = ('id', 'sensors', 'uid', 'owner', 'location', 'last_data_push')
+
+    def get_last_data_push(self, obj):
+        x = Node.objects.get(pk=5).sensors.order_by('-sensordatas__timestamp')[:1].values_list('sensordatas__timestamp', flat=True)
+        return x[0] if x else None
