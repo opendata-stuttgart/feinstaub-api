5c49c98aaf0da71c063f6d25ec49bef4d31db572
Andreas Madsack <andreas@madflex.de>
2015-10-04 22:01:50 +0200

    add django-filter

--- feinstaub/feinstaub/settings/base.py
+++ feinstaub/feinstaub/settings/base.py
@@ -98,5 +98,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.DjangoFilterBackend',
+    ),
 }
