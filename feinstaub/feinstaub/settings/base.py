6586a13da995d076dc6ccb2e1df7c8ddaa88c5d9
Andreas Madsack <andreas@madflex.de>
2015-12-03 14:02:50 +0100

    fix settings to show branding

--- feinstaub/feinstaub/settings/base.py
+++ feinstaub/feinstaub/settings/base.py
@@ -103,3 +103,20 @@ REST_FRAMEWORK = {
         'rest_framework.filters.DjangoFilterBackend',
     ),
 }
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.normpath(os.path.join(SITE_ROOT, 'templates'))],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
