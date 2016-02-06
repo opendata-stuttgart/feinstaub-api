f2eebd8899c3563c7659890369e335a1c2f0ad51
Michael Käufl <opendata-stuttgart@c.michael-kaeufl.de>
2016-02-06 23:26:18 +0100

    [sensors] Used itertools.product
    - Sensor.objects.all() is only called once
    - less indentation

--- feinstaub/sensors/management/commands/export_as_csv.py
+++ feinstaub/sensors/management/commands/export_as_csv.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 import os
-from django.core.management import BaseCommand
 import datetime
+from itertools import product
+
+from django.core.management import BaseCommand
 
 
 def str2date(str, default):
@@ -30,68 +32,67 @@ class Command(BaseCommand):
 
         folder = "/opt/code/archive"
 
-        for dt in self._dates(start_date, end_date):
-            for sensor in Sensor.objects.all():
-                # first only for ppd42ns.
-                # because we need a list of fields for all other sensors
-                # -> SENSOR_TYPE_CHOICES needs to become more sophisticated
-                if not sensor.sensor_type.name.lower() == "ppd42ns":
-                    continue
+        for dt, sensor in product(self._dates(start_date, end_date), Sensor.objects.all()):
+            # first only for ppd42ns.
+            # because we need a list of fields for all other sensors
+            # -> SENSOR_TYPE_CHOICES needs to become more sophisticated
+            if not sensor.sensor_type.name.lower() == "ppd42ns":
+                continue
 
-                fn = "{date}_{stype}_sensor_{sid}.csv".format(
-                    sid=sensor.id,
-                    stype=sensor.sensor_type.name.lower(),
-                    date=str(dt),
-                )
+            fn = "{date}_{stype}_sensor_{sid}.csv".format(
+                sid=sensor.id,
+                stype=sensor.sensor_type.name.lower(),
+                date=str(dt),
+            )
 
-                # location 11 is the dummy location. remove the datasets.
-                # remove all indoor locations
-                qs = SensorData.objects \
-                    .filter(sensor=sensor) \
-                    .exclude(location_id=11) \
-                    .exclude(location__indoor=True) \
-                    .filter(timestamp__date=dt) \
-                    .order_by("timestamp")
-                if not qs.exists():
-                    continue
+            # location 11 is the dummy location. remove the datasets.
+            # remove all indoor locations
+            qs = SensorData.objects \
+                .filter(sensor=sensor) \
+                .exclude(location_id=11) \
+                .exclude(location__indoor=True) \
+                .filter(timestamp__date=dt) \
+                .order_by("timestamp")
+            if not qs.exists():
+                continue
 
-                print(fn)
-                os.makedirs(os.path.join(folder, str(dt)), exist_ok=True)
+            print(fn)
+            os.makedirs(os.path.join(folder, str(dt)), exist_ok=True)
 
-                # if file exists; overwrite. always
-                with open(os.path.join(folder, str(dt), fn), "w") as fp:
-                    fp.write("sensor_id;sensor_type;location;lat;lon;timestamp;")
-                    # FIXME: generate from SENSOR_TYPE_CHOICES
-                    fp.write("P1;durP1;ratioP1;P2;durP2;ratioP2\n")
-                    for sd in qs:
-                        try:
-                            p1 = sd.sensordatavalues.get(value_type="P1").value
-                        except SensorDataValue.DoesNotExist:
-                            # FIXME: log defect datasets!
-                            continue
+            # if file exists; overwrite. always
+            with open(os.path.join(folder, str(dt), fn), "w") as fp:
+                fp.write("sensor_id;sensor_type;location;lat;lon;timestamp;")
+                # FIXME: generate from SENSOR_TYPE_CHOICES
+                fp.write("P1;durP1;ratioP1;P2;durP2;ratioP2\n")
+                for sd in qs:
+                    try:
+                        p1 = sd.sensordatavalues.get(value_type="P1").value
+                    except SensorDataValue.DoesNotExist:
+                        # FIXME: log defect datasets!
+                        continue
 
-                        longitude = ''
-                        if sd.location.longitude:
-                            longitude = "{:.3f}".format(sd.location.longitude)
-                        latitude = ''
-                        if sd.location.latitude:
-                            latitude = "{:.3f}".format(sd.location.latitude)
-                        s = ';'.join([str(sensor.id),
-                                      sensor.sensor_type.name,
-                                      str(sd.location.id),
-                                      latitude,
-                                      longitude,
-                                      sd.timestamp.isoformat()])
+                    longitude = ''
+                    if sd.location.longitude:
+                        longitude = "{:.3f}".format(sd.location.longitude)
+                    latitude = ''
+                    if sd.location.latitude:
+                        latitude = "{:.3f}".format(sd.location.latitude)
+                    s = ';'.join([str(sensor.id),
+                                  sensor.sensor_type.name,
+                                  str(sd.location.id),
+                                  latitude,
+                                  longitude,
+                                  sd.timestamp.isoformat()])
 
-                        fp.write(s)
-                        fp.write(';')
-                        fp.write('{};'.format(p1))
-                        fp.write('{};'.format(sd.sensordatavalues.get(value_type="durP1").value))
-                        fp.write('{};'.format(sd.sensordatavalues.get(value_type="ratioP1").value))
-                        fp.write('{};'.format(sd.sensordatavalues.get(value_type="P2").value))
-                        fp.write('{};'.format(sd.sensordatavalues.get(value_type="durP2").value))
-                        fp.write('{}'.format(sd.sensordatavalues.get(value_type="ratioP2").value))
-                        fp.write("\n")
+                    fp.write(s)
+                    fp.write(';')
+                    fp.write('{};'.format(p1))
+                    fp.write('{};'.format(sd.sensordatavalues.get(value_type="durP1").value))
+                    fp.write('{};'.format(sd.sensordatavalues.get(value_type="ratioP1").value))
+                    fp.write('{};'.format(sd.sensordatavalues.get(value_type="P2").value))
+                    fp.write('{};'.format(sd.sensordatavalues.get(value_type="durP2").value))
+                    fp.write('{}'.format(sd.sensordatavalues.get(value_type="ratioP2").value))
+                    fp.write("\n")
 
     @staticmethod
     def _dates(start, end):
