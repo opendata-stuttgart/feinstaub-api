7a7feb5a26b6bdfc9d2a1c839d31b747cc4ba03b
Michael Käufl <opendata-stuttgart@c.michael-kaeufl.de>
2017-02-19 01:41:00 +0100

    [sensors] Removed unused functions
    `sensordata.sensor.owner.username` does not work.  If anyone who uses
    the functions would have fixed it.

--- feinstaub/sensors/utils.py
+++ feinstaub/sensors/utils.py
@@ -1,60 +1,8 @@
-from django.core.cache import cache
-import pandas as pd
-
 from .models import (
     SensorData,
     SensorDataValue,
-    SensorLocation,
     SENSOR_TYPE_CHOICES,
 )
-
-
-def calculate_datatable():
-    for location in SensorLocation.objects.all():
-        calculate_datatable_location(location)
-
-
-def calculate_datatable_location(location):
-    # calculate for each location per day a table with all sensor values
-    table = sensordata_to_dataframe(location)
-
-    data = {
-        'location': {
-            'location': location.location,
-            'indoor': location.indoor,
-            'owner': location.owner.username,
-            'description': location.description,
-        },
-        'table_head': table.columns,
-        'table': table.values,
-    }
-
-    cache.set(
-        'location_cache_{}'.format(location.pk),
-        data,
-        timeout=None,
-    )
-
-
-def sensordata_to_dataframe(location):
-    data_list = [
-        {
-            'timestamp': [str(sensordata.timestamp)],
-            'sensor': [sensordata.id],
-            'value_type': [datavalue.value_type],
-            'value': [datavalue.value],
-            'sensor__owner': [sensordata.sensor.owner.username],
-            'sampling_rate': [sensordata.sampling_rate],
-        }
-        for sensordata in SensorData.objects.filter(location=location)
-        for datavalue in sensordata.sensordatavalues.all()
-    ]
-
-    return pd.DataFrame(
-        data_list,
-        columns=['timestamp', 'sensor', 'value_type', 'value', 'sensor__owner',
-                 'sampling_rate'],
-    )
 
 
 def export_to_csv():
