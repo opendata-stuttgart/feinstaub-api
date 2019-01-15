9a949fd0394d2af64dad66ded4cd11f735585c9b
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-01-15 12:05:45 +0300

    Making it possible to install feinstaub as a package (#7)
    * added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="feinstaub",
+    version="0.0.1",
+    author="Feinstaub",
+    description="Api to save data from sensors (especially particulates sensors).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opendata-stuttgart/feinstaub-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires
+)
